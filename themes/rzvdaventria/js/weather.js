e5745b5e04039605dc6059255aeb1d589247509f
Christiaan ter Veen <mail@rork.nl>
2024-09-08 10:26:14 +0200

    Update weather.js
    Coding standaard
    Bug met timestamp wanneer de observatie is verlopen.

--- themes/rzvdaventria/js/weather.js
+++ themes/rzvdaventria/js/weather.js
@@ -1,28 +1,33 @@
-(function ($, Drupal, once) {
-  Drupal.behaviors.myModuleBehavior = {
-    attach: function (context, settings) {
-      once('alt-weather', '#block-rzvdaventria-weerenwaterstanden').forEach(function (element) {
-        // Apply the myCustomBehaviour effect to the elements only once.
-        $.getJSON( "http://192.168.2.6/~internetcie/webcam/data/weerdata.json", function( data ) {
-          $.each( data, function ( key, val ) {
-            var value = new String( val.value );
-                value = value.replace(".", ",");
+/**
+ * @file
+ * Alt-text for the weather icons
+ */
+(() => {
+  (function ($, Drupal, once) {
+    Drupal.behaviors.myModuleBehavior = {
+      attach: function (context, settings) {
+        once('alt-weather', '#block-rzvdaventria-weerenwaterstanden').forEach(function (element) {
+          $.getJSON( "http://192.168.2.6/~internetcie/webcam/data/weerdata.json", function( data ) {
+            $.each( data, function ( key, val ) {
+              var timestamp = new String( val.timestamp );
+              var value = new String( val.value );
+                  value = value.replace(".", ",");
 
-            var now = new Date ();
-            var expires = new Date( Date.parse( val.expires ) );
-            if (now > expires ) {
-              value = "X";
-              hour = "--";
-              minutes = "--";
-            }
+              var now = new Date ();
+              var expires = new Date( Date.parse( val.expires ) );
+              if (now > expires ) {
+                value = "X";
+                timestamp = "--:--";
+              }
 
-            var label = val.label + ": " + value + val.unit + " (" + val.timestamp + "u)";
-            var id = "#" + key;
+              var label = val.label + ": " + value + val.unit + " (" + timestamp + "u)";
+              var id = "#" + key;
 
-            $( id ).attr( "alt", label );
+              $( id ).attr( "alt", label );
+            });
           });
         });
-      });
-    }
-  };
-})(jQuery, Drupal, once);
+      }
+    };
+  })(jQuery, Drupal, once);
+})();
