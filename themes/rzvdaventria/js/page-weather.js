4f2e92c4e4a09d21e8bbce3dd71ecd3ddaf8014d
Christiaan ter Veen <mail@rork.nl>
2024-10-18 19:52:48 +0200

    Update page-weather.js

--- themes/rzvdaventria/js/page-weather.js
+++ themes/rzvdaventria/js/page-weather.js
@@ -6,7 +6,7 @@
   (function ($, Drupal, once) {
     Drupal.behaviors.rzvdaventriaWeatherGraphsBehavior = {
       attach: function (context, settings) {
-        once('weather-graphs', '#rzvdaventria-graph-watertemperatuur').forEach(function (element) {
+        once('graph-watertemperatuur', '#rzvdaventria-graph-watertemperatuur').forEach(function (element) {
           $.getJSON("http://192.168.2.6/~internetcie/webcam/data/watertemperatuur_drielboven.json", function(data) {
             let yrange = rzvdaventriaMinMax(data.gemeten.waarde);
             let now = new Date();
@@ -53,9 +53,6 @@
                 },
                 xaxis: {
                   autorange: false,
-                  title: {
-                    text: "Dag",
-                  },
                   type: "date",
                   range: xrange
                 },
@@ -63,6 +60,73 @@
                   autorange: false,
                   title: {
                     text: "Temperatuur (&deg;C)",
+                  },
+                  range: yrange
+                },
+                showlegend: false
+              }
+            );
+          });
+        });
+
+        once('graph-ijsselpeil', '#rzvdaventria-graph-ijsselpeil').forEach(function (element) {
+          $.getJSON("http://192.168.2.6/~internetcie/webcam/data/ijsselpeil_deventer.json", function(data) {
+            let yrange = rzvdaventriaMinMax(data.gemeten.waarde);
+            let now = new Date();
+            let now_label = " " + now.getHours() + ":" + (now.getMinutes() < 10 ? 0 : "") + now.getMinutes();
+            let xmin = new Date(now.getTime() - 7*24*60*60*1000);
+            let xmax = new Date(now.getTime() + 2*24*60*60*1000);
+            let xrange = Array(xmin, xmax);
+            let now_x = Array(now, now);
+            let lastValue = data.gemeten.waarde[data.gemeten.waarde.length-1]
+            let lastTimestamp = new Date(data.gemeten.tijdstip[data.gemeten.tijdstip.length-1]);
+            let element = document.getElementById('rzvdaventria-graph-ijsselpeil');
+
+            yrange[0] -= 1;
+            yrange[1] += 1;
+
+            $( "#caption-ijsselpeil" ).html(rzvdaventriaWaterdataCaption(lastValue, data.unit, lastTimestamp));
+
+            Plotly.newPlot(element,
+              data = [
+                {
+                  x: data.gemeten.tijdstip,
+                  y: data.gemeten.waarde
+                },
+                {
+                  x: data.voorspeld.tijdstip,
+                  y: data.voorspeld.waarde
+                },
+                {
+                  x: [now, now],
+                  y: yrange,
+                  mode: "lines"
+                },
+                {
+                  x: [now],
+                  y: [yrange[0]],
+                  mode: "text",
+                  text: [now_label],
+                  textposition: "top right"
+                },
+              ],
+              layout = {
+                margin: {
+                  t: 0,
+                  r: 0,
+                  b: 80,
+                  l: 80,
+                  pad: 10
+                },
+                xaxis: {
+                  autorange: false,
+                  type: "date",
+                  range: xrange
+                },
+                yaxis: {
+                  autorange: false,
+                  title: {
+                    text: "Waterhoogte (m)",
                   },
                   range: yrange
                 },
