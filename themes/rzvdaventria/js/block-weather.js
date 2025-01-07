580b50e43151cd222b27921eb7fff8196f08d708
Christiaan ter Veen <mail@rork.nl>
2025-01-07 14:06:25 +0100

    Update block-weather.js
    ijsselpeil losgemaakt i.v.m. ander databestand

--- themes/rzvdaventria/js/block-weather.js
+++ themes/rzvdaventria/js/block-weather.js
@@ -35,6 +35,33 @@
               $(id).attr("title", title);
             });
           });
+
+          $.getJSON("http://192.168.2.6/~internetcie/webcam/data/ijsselpeil_deventer.json", function(data) {
+            let expires = new Date(Date.parse(data.actueel.expires));
+            let id = "#ijsselpeil";
+            let alt = "";
+            let title = "";
+            let now = new Date ();
+            let timestamp = data.actueel.timestamp;
+            let value = data.actueel.waarde;
+            let caption = data.actueel.caption;
+
+            if (now.getTime() > expires.getTime()) {
+              value = "X";
+              timestamp = "--:--";
+              caption = "";
+            }
+
+            alt = "IJsselpeil Deventer: " + value + data.unit + " (" + timestamp + "u)";
+            if (caption != "") {
+              alt += " " + caption;
+            }
+
+            title = "IJsselpeil Deventer (" + timestamp + "u)";
+
+            $(id).attr("alt", alt);
+            $(id).attr("title", title);
+          });
         });
       }
     };
