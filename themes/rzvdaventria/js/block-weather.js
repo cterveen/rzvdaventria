d52a55df93ecc2f53dfa1f3f613ff796b459dd98
Christiaan ter Veen <mail@rork.nl>
2025-01-17 18:37:17 +0100

    Update block-weather.js
    Workaround for windkracht.
    Waterpeil op twee cijfers achter de komma.

--- themes/rzvdaventria/js/block-weather.js
+++ themes/rzvdaventria/js/block-weather.js
@@ -9,6 +9,9 @@
         once('alt-weather', '#block-rzvdaventria-weerenwaterstanden').forEach(function (element) {
           $.getJSON("http://192.168.2.6/~internetcie/webcam/data/weerdata.json", function(data) {
             $.each(data, function(key, val) {
+              if (key != "windkracht" & key != "temperatuur") {
+                return true;
+              }
               let expires = new Date(Date.parse(val.expires));
               let id = "#" + key;
               let alt = "";
@@ -18,13 +21,20 @@
               let value = val.value;
               let caption = val.caption;
 
+              if (key == "windkracht") {
+                val.unit = "bft";
+                expires = new Date(Date.parse(val.values[99][0]));
+                timestamp = val.values[99][1];
+                value = val.values[99][2].toFixed(1);
+              }
+
               if (now.getTime() > expires.getTime()) {
                 value = "X";
                 timestamp = "--:--";
                 caption = "";
               }
 
-              alt = val.label + ": " + value + val.unit + " (" + timestamp + "u)";
+              alt = val.label + ": " + value + " " + val.unit + " (" + timestamp + "u)";
               if (caption != "") {
                 alt += " " + caption;
               }
@@ -43,7 +53,7 @@
             let title = "";
             let now = new Date ();
             let timestamp = data.actueel.timestamp;
-            let value = data.actueel.waarde;
+            let value = data.actueel.waarde.toFixed(2);
             let caption = data.actueel.caption;
 
             if (now.getTime() > expires.getTime()) {
@@ -52,7 +62,7 @@
               caption = "";
             }
 
-            alt = "IJsselpeil Deventer: " + value + data.unit + " (" + timestamp + "u)";
+            alt = "IJsselpeil Deventer: " + value + " " + data.unit + " (" + timestamp + "u)";
             if (caption != "") {
               alt += " " + caption;
             }
