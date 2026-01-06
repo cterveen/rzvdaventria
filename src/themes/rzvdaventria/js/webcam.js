ee24d18b2f275ca40284a3c10edf441a660c2a0e
Christiaan ter Veen <mail@rork.nl>
2026-01-06 15:18:59 +0100

    Update webcam.js
    Padding of hours and numbers
    Also update webcam_haven on the weather page.

--- src/themes/rzvdaventria/js/webcam.js
+++ src/themes/rzvdaventria/js/webcam.js
@@ -8,18 +8,19 @@
       attach: function (context, settings) {
         once('alt-webcam', '#block-rzvdaventria-webcammetlinknaardeweerpagina').forEach(function (element) {
           $.get(window.location.origin + "/webcam/data/webcam_haven.jpg", function( data, status, xhr) {
-            let id = "#webcam_haven";
             let lastModified = new Date(Date.parse(xhr.getResponseHeader('Last-Modified')));
             let expires = new Date(lastModified.getTime() + (60 * 60 * 1000));
             let now = new Date();
             let label = "Webcam vlot (--.--u)";
 
             if (now.getTime() < expires.getTime()) {
-              label = "Webcam vlot (" + lastModified.getHours() + ":" + lastModified.getMinutes() + "u)";
+              label = "Webcam vlot (" + String(lastModified.getHours()).padStart(2, "0") + ":" + String(lastModified.getMinutes()).padStart(2, "0") + "u)";
             }
 
-            $( id ).attr( "alt", label );
-            $( id ).attr( "title", label );
+            $( "#webcam_haven" ).attr( "alt", label );
+            $( "#webcam_haven" ).attr( "title", label );
+            $( "#webcam_haven2" ).attr( "alt", label );
+            $( "#webcam_haven2" ).attr( "title", label );
           });
         });
       }
